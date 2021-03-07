3a3bc7ed4b4e6cdb37e4a3406bc993f3cbeef62e
Andrei <andrew162555@gmail.com>
2021-03-08 02:34:11 +0300

    Закрепление условий

--- livelive/js/script2.js
+++ livelive/js/script2.js
@@ -18,19 +18,47 @@
 
 // (num === 50) ? console.log("Ok!") : console.log("Error");
 
-const num = 50;
+// const num = 50;
 
-switch (num) {
-    case 49:
-        console.log("Error");
-        break;
-    case 100:
-        console.log("Error");
-        break;
-    case 51:
-        console.log("True!!");
-        break;
-    default:
-        console.log("Not this time))");
-        break;
+// switch (num) {
+//     case 49:
+//         console.log("Error");
+//         break;
+//     case 100:
+//         console.log("Error");
+//         break;
+//     case 51:
+//         console.log("True!!");
+//         break;
+//     default:
+//         console.log("Not this time))");
+//         break;
+// };
+
+// let theNumber = Number(prompt("Enter number"));
+
+// if (!Number.isNaN(theNumber)) {
+//     console.log("Это число является корнем числа " + theNumber * theNumber);
+// }
+// else {
+//     console.log("Hey why you didnt enter a number?");
+// };
+
+// let justNumber = Number(prompt("Enter the number: "));
+
+// if (justNumber < 10) {
+//     console.log("You entered less then 10 " + justNumber );
+// } else if (justNumber < 100) {
+//     console.log("You entered less then 100 " + justNumber);
+// } else if (justNumber < 1000) {
+//     console.log("You entered a number less then 1000 " + justNumber);
+// } else {
+//     console.log("WHy you entered not a number?? bitch");
+// };
+
+let number = 0;
+
+while (number <= 12) {
+    console.log(number);
+    number += 2;
 };
